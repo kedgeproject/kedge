ac1a6e9af1ccaf45bc622388e43033a25b16abd8
Suraj Deshmukh <surajd.service@gmail.com>
2017-10-04 17:54:13 +0530

    (feat): build docker images using, `kedge build`
    Now user can do `kedge build -i <image_name>` the image will be built
    for the user.
    
    If user wants the image to be pushed to the image registry she can
    provide flag `-p` to do. Image will be pushed to registry that she
    is logged in to.

---
+++ cmd/build.go
@@ -0,0 +1,56 @@
+/*
+Copyright 2017 The Kedge Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kedgeproject/kedge/pkg/build"
+
+	"github.com/spf13/cobra"
+)
+
+var Dockerfile string
+var DockerImage string
+var DockerContext string
+var PushImage bool
+
+var buildCmd = &cobra.Command{
+	Use:   "build",
+	Short: "build application image",
+	Run: func(cmd *cobra.Command, args []string) {
+		if DockerImage == "" {
+			fmt.Println("Please specify the container image name using flag '--image' or '-i'")
+			os.Exit(-1)
+		}
+		if err := build.BuildPushDockerImage(Dockerfile, DockerImage, DockerContext, PushImage); err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
+	},
+}
+
+func init() {
+
+	buildCmd.Flags().StringVarP(&Dockerfile, "file", "f", "Dockerfile", "Specify Dockerfile for doing builds, Dockerfile path is relative to context")
+	buildCmd.Flags().StringVarP(&DockerImage, "image", "i", "", "Image name and tag of resulting image")
+	buildCmd.Flags().StringVarP(&DockerContext, "context", "c", ".", "Path to a directory containing a Dockerfile, it is build context that is sent to the Docker daemon")
+	buildCmd.Flags().BoolVarP(&PushImage, "push", "p", false, "Add this flag if you want to push the image")
+
+	RootCmd.AddCommand(buildCmd)
+}
---
+++ pkg/build/build.go
@@ -0,0 +1,175 @@
+/*
+Copyright 2017 The Kedge Authors All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package build
+
+import (
+	"archive/tar"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+	dockerlib "github.com/fsouza/go-dockerclient"
+	"github.com/pkg/errors"
+)
+
+func BuildPushDockerImage(dockerfile, image, context string, push bool) error {
+
+	context, err := filepath.Abs(context)
+	if err != nil {
+		return err
+	}
+	// Connect to the Docker client
+	client, err := dockerlib.NewClientFromEnv()
+	if err != nil {
+		return err
+	}
+
+	build := Build{Client: *client}
+	if err = build.BuildImage(dockerfile, image, context); err != nil {
+		return err
+	}
+
+	if push {
+		if err = PushImage(image); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Build will provide methods for interaction with API regarding building images
+type Build struct {
+	Client dockerlib.Client
+}
+
+/*
+BuildImage builds a Docker image via the Docker API. Takes the source directory
+and image name and then builds the appropriate image. Tarball is utilized
+in order to make building easier.
+*/
+func (c *Build) BuildImage(dockerfile, image, context string) error {
+
+	log.Infof("Building image '%s' from directory '%s'", image, path.Base(context))
+
+	// Create a temporary file for tarball image packaging
+	tmpFile, err := ioutil.TempFile("/tmp", "kedge-image-build-")
+	if err != nil {
+		return err
+	}
+	log.Debugf("Created temporary file %v for Docker image tarballing", tmpFile.Name())
+
+	// Create a tarball of the source directory in order to build the resulting image
+	err = CreateTarball(strings.Join([]string{context, ""}, "/"), tmpFile.Name())
+	if err != nil {
+		return errors.Wrap(err, "unable to create a tarball")
+	}
+
+	// Load the file into memory
+	tarballSource, err := os.Open(tmpFile.Name())
+	if err != nil {
+		return errors.Wrap(err, "unable to load tarball into memory")
+	}
+
+	// Let's create all the options for the image building.
+	outputBuffer := bytes.NewBuffer(nil)
+	opts := dockerlib.BuildImageOptions{
+		Name:         image,
+		InputStream:  tarballSource,
+		OutputStream: outputBuffer,
+		Dockerfile:   dockerfile,
+	}
+
+	// Build it!
+	if err := c.Client.BuildImage(opts); err != nil {
+		return errors.Wrap(err, "unable to build image")
+	}
+
+	log.Infof("Image '%s' from directory '%s' built successfully", image, path.Base(context))
+	log.Debugf("Image %s build output:\n%s", image, outputBuffer)
+
+	return nil
+}
+
+/*
+CreateTarball creates a tarball for source and dumps it to target path
+
+Function modified and added from https://github.com/mholt/archiver/blob/master/tar.go
+*/
+func CreateTarball(source, target string) error {
+	tarfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer tarfile.Close()
+
+	tarball := tar.NewWriter(tarfile)
+	defer tarball.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	return filepath.Walk(source,
+		func(path string, info os.FileInfo, err error) error {
+			if baseDir == path {
+				return nil
+			}
+			if err != nil {
+				return err
+			}
+			header, err := tar.FileInfoHeader(info, info.Name())
+			if err != nil {
+				return err
+			}
+
+			if baseDir != "" {
+				if strings.HasSuffix(source, "/") {
+					header.Name = strings.TrimPrefix(path, source)
+				} else {
+					header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+				}
+			}
+
+			if err := tarball.WriteHeader(header); err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			_, err = io.Copy(tarball, file)
+			return err
+		})
+}
