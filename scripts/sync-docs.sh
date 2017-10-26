60db73539c47fbbda7ac271982c83dae35940c28
Charlie Drage <charlie@charliedrage.com>
2017-10-26 15:03:21 -0400

    Update encrypted deploy key (#393)
    Updates the encrypted deploy key to the most current one.

--- scripts/sync-docs.sh
+++ scripts/sync-docs.sh
@@ -8,16 +8,14 @@ fi
 
 DOCS_REPO_NAME="kedge"
 DOCS_REPO_URL="git@github.com:kedgeproject/kedge.git"
-DOCS_KEY="scripts/deploy_key.enc"
+DOCS_KEY="scripts/deploy_key"
 DOCS_USER="kedge-bot"
 DOCS_EMAIL="cdrage+kedge@redhat.com"
 DOCS_BRANCH="gh-pages"
 DOCS_FOLDER="docs"
-ENC_KEY=$encrypted_91569b511922_key
-ENC_IV=$encrypted_91569b511922_iv
 
 # decrypt the private key
-openssl aes-256-cbc -K $ENC_KEY -iv $ENC_IV -in "$DOCS_KEY" -out "$DOCS_KEY" -d
+openssl aes-256-cbc -K $encrypted_91569b511922_key -iv $encrypted_91569b511922_iv -in "$DOCS_KEY.enc" -out "$DOCS_KEY" -d
 chmod 600 "$DOCS_KEY"
 eval `ssh-agent -s`
 ssh-add "$DOCS_KEY"
